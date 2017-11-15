a33584a795139d0dbf637acdeca7ca62b4204217
HeyItzGinger <tumtumjmh@gmail.com>
2017-11-14 18:58:20 -0800

    Module 3 checkpoint

---
+++ src/CancerSong.java
@@ -0,0 +1,19 @@
+
+public class CancerSong {
+public static void main(String[] args) {
+	String Spud = "Potato";
+	for (int i = 1; i < 20; i++) {
+		if (i < 4) {
+			System.out.println(i + " " + Spud+"!");
+		} else if (i == 5) {
+			System.out.println("4!");
+		}
+	 else if (i > 4 && i < 9) {
+		System.out.println(i+" "+Spud+"!");
+	} else if (i == 9) {
+		System.out.println("More!");
+		
+	}
+}
+}
+}
---
+++ src/OddlyLoopy.java
@@ -0,0 +1,20 @@
+
+public class OddlyLoopy {
+	public static void main(String[] args) {
+		for (int i = 0; i < 500/2; i++) {
+			System.out.println((i*2)+1);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
